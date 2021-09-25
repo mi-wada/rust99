9175f3124607206dfbf5567eb348b1ec865bc881
mi-wada <mtak.wd@gmail.com>
2021-09-25 20:53:36 +0900

    solve p02

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ mod tests {
     #[test]
     fn p01() {
         let li = vec![1, 1, 2, 3, 5, 8];
-        fn last(v: &Vec<i32>) -> Option<&i32> {
-            v.last()
+        fn last(v: &Vec<i32>) -> Option<i32> {
+            v.last().copied()
         }
-        assert_eq!(Some(&8), last(&li));
+        assert_eq!(Some(8), last(&li));
+    }
+
+    #[test]
+    fn p02() {
+        let li = vec![1, 1, 2, 3, 5, 8];
+        fn penultimate(v: &Vec<i32>) -> Option<i32> {
+            v[..v.len() - 1].last().copied()
+        }
+        assert_eq!(Some(5), penultimate(&li));
     }
 }
